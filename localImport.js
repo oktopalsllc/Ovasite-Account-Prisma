91266a0c101c748fbce616c0114dc45e621fb93e
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-08-24 15:18:49 +0100

    feat: implementing security libraries (helmet,cors,csurf)

--- localImport.js
+++ localImport.js
@@ -2,5 +2,6 @@
 import connectDB from "./config/connectDB.js";
 import authRouter from "./auth/auth.routes.js";
 import userRouter from "./users/users.routes.js";
+import organizationRouter from "./organization/organization.routes.js";
 
-export { connectDB, authRouter, userRouter };
+export { connectDB, authRouter, userRouter, organizationRouter };
--- server.js
+++ server.js
@@ -2,7 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { connectDB, authRouter, userRouter } from "./localImport.js";
+import csurf from "csurf";
+import {
+  connectDB,
+  authRouter,
+  userRouter,
+  organizationRouter,
+} from "./localImport.js";s
 dotenv.config();
 
 const app = express();
@@ -11,6 +17,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
+app.use(csurf({ cookie: true }));
+app.use(helmet());
 
 //Routes go here
 app.get("/", (req, res) => {
@@ -20,6 +29,7 @@ app.get("/", (req, res) => {
 const basePath = "/api/v1";
 app.use(`${basePath}/auth`, authRouter);
 app.use(`${basePath}/users`, userRouter);
+app.use(`${basePath}/organization`, organizationRouter);
 
 //Connect to the database before listening
 const PORT = process.env.PORT || process.env.API_PORT;
