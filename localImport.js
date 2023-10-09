f5fe6a0d78f07815043ebe6d21542bf895e013e3
Michael <gadreelazazel@gmail.com>
2023-10-09 22:27:15 +0100

    Update

--- localImport.js
+++ localImport.js
@@ -7,10 +7,10 @@ import employeeRouter from "./employees/employees.routes.js";
 import teamsRouter from "./teams/teams.routes.js";
 import inviteRouter from "./invites/invites.routes.js";
 import organizationsRouter from "./organizations/organizations.routes.js";
-import projectRouter from "./projects/projects.routes.js";
-import formRouter from "./form/form.routes.js";
-import submissionRouter from "./submissions/submissions.routes";
-import reportRouter from "./reports/reports.routes";
+import projectRouter from "./projects/project.routes.js";
+import formRouter from "./forms/form.routes.js";
+import submissionRouter from "./submissions/submission.routes";
+import reportRouter from "./reports/report.routes";
 
 export {
   connectDB,
