96c463ffaa5beaff007c925501c2f3b10884439d
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-10-15 17:22:47 +0100

    fix: removed connectDB from local import

--- localImport.js
+++ localImport.js
@@ -1,27 +1,25 @@
 // local imports
-import connectDB from "./config/connectDB.js";
 import authRouter from "./auth/auth.routes.js";
 import userRouter from "./users/users.routes.js";
 
 import employeeRouter from "./employees/employees.routes.js";
-import teamsRouter from "./teams/teams.routes.js";
+import formRouter from "./forms/form.routes.js";
 import inviteRouter from "./invites/invites.routes.js";
 import organizationsRouter from "./organizations/organizations.routes.js";
 import projectRouter from "./projects/project.routes.js";
-import formRouter from "./forms/form.routes.js";
-import submissionRouter from "./submissions/submission.routes.js";
 import reportRouter from "./reports/report.routes.js";
+import submissionRouter from "./submissions/submission.routes.js";
+import teamsRouter from "./teams/teams.routes.js";
 
 export {
-  connectDB,
   authRouter,
-  userRouter,
-  organizationsRouter,
   employeeRouter,
-  teamsRouter,
-  inviteRouter,
-  projectRouter,
   formRouter,
+  inviteRouter,
+  organizationsRouter,
+  projectRouter,
+  reportRouter,
   submissionRouter,
-  reportRouter
+  teamsRouter,
+  userRouter,
 };
