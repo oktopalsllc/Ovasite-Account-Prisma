adbcce5f3c99aa8f8e8b1d03206efbeceb4fb439
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-10-24 18:49:07 +0100

    feat: updated middle imports on the different modules

--- forms/form.routes.js
+++ forms/form.routes.js
@@ -1,30 +1,25 @@
-import {
-    createForm,
-    getForm,
-    getForms,
-    getFormsByEmployee,
-    updateForm,
-    deleteForm
-} from './form.controller.js';
 import express from "express";
-import { 
-    checkOrganizationExists 
-} from '../organizations/organizations.middleware.js';
+import { verifyUser } from "../middleware/authenticate.js";
+import { checkOrganizationExists } from "../organizations/organizations.middleware.js";
 import {
-    verifyAdmin,
-    verifyUser
-} from "../middleware/authenticate.js";
+  createForm,
+  deleteForm,
+  getForm,
+  getForms,
+  getFormsByEmployee,
+  updateForm,
+} from "./form.controller.js";
 
 const formRouter = express.Router({ mergeParams: true });
 
-formRouter.use('/:orgId', verifyUser);
-formRouter.use('/:orgId', checkOrganizationExists);
+formRouter.use("/:orgId", verifyUser);
+formRouter.use("/:orgId", checkOrganizationExists);
 
-formRouter.post('/:orgId/form/create', createForm);
-formRouter.get('/:orgId/form/:formId', getForm);
-formRouter.get('/:orgId/forms/:projectId', getForms);
-formRouter.get('/:orgId/userforms/:creatorId/:projectId', getFormsByEmployee);
-formRouter.patch('/:orgId/form/update/:formId', updateForm);
-formRouter.delete('/:orgId/form/delete/:formId', deleteForm);
+formRouter.post("/:orgId/form/create", createForm);
+formRouter.get("/:orgId/form/:formId", getForm);
+formRouter.get("/:orgId/forms/:projectId", getForms);
+formRouter.get("/:orgId/userforms/:creatorId/:projectId", getFormsByEmployee);
+formRouter.patch("/:orgId/form/update/:formId", updateForm);
+formRouter.delete("/:orgId/form/delete/:formId", deleteForm);
 
 export default formRouter;
--- submissions/submission.routes.js
+++ submissions/submission.routes.js
@@ -1,35 +1,42 @@
 import {
-    createSubmission,
-    getSubmission,
-    getSubmissions,
-    getFormSubmissions,
-    getEmployeeSubmissions,
-    updateSubmission,
-    exportSubmission,
-    deleteSubmission
-} from './submission.controller.js';
+  createSubmission,
+  deleteSubmission,
+  exportSubmission,
+  getEmployeeSubmissions,
+  getFormSubmissions,
+  getSubmission,
+  getSubmissions,
+  updateSubmission,
+} from "./submission.controller.js";
 
 import express from "express";
-import { 
-    checkOrganizationExists 
-} from '../organizations/organizations.middleware.js';
-import {
-    verifyAdmin,
-    verifyUser
-} from "../middleware/authenticate.js";
+import { verifyUser } from "../middleware/authenticate.js";
+import { checkOrganizationExists } from "../organizations/organizations.middleware.js";
 
 const submissionRouter = express.Router({ mergeParams: true });
 
-submissionRouter.use('/:orgId', verifyUser);
-submissionRouter.use('/:orgId', checkOrganizationExists);
+submissionRouter.use("/:orgId", verifyUser);
+submissionRouter.use("/:orgId", checkOrganizationExists);
 
-submissionRouter.post('/:orgId/submission/create', createSubmission);
-submissionRouter.get('/:orgId/submission/:submissionId', getSubmission);
-submissionRouter.get('/:orgId/submissions/:projectId', getSubmissions);
-submissionRouter.get('/:orgId/submissions/:formId', getFormSubmissions);
-submissionRouter.get('/:orgId/usersubmissions/:employeeId/:projectId', getEmployeeSubmissions);
-submissionRouter.patch('/:orgId/submission/update/:submissionId', updateSubmission);
-submissionRouter.get('/:orgId/submission/export/:submissionId', exportSubmission);
-submissionRouter.delete('/:orgId/submission/delete/:submissionId', deleteSubmission);
+submissionRouter.post("/:orgId/submission/create", createSubmission);
+submissionRouter.get("/:orgId/submission/:submissionId", getSubmission);
+submissionRouter.get("/:orgId/submissions/:projectId", getSubmissions);
+submissionRouter.get("/:orgId/submissions/:formId", getFormSubmissions);
+submissionRouter.get(
+  "/:orgId/usersubmissions/:employeeId/:projectId",
+  getEmployeeSubmissions
+);
+submissionRouter.patch(
+  "/:orgId/submission/update/:submissionId",
+  updateSubmission
+);
+submissionRouter.get(
+  "/:orgId/submission/export/:submissionId",
+  exportSubmission
+);
+submissionRouter.delete(
+  "/:orgId/submission/delete/:submissionId",
+  deleteSubmission
+);
 
 export default submissionRouter;
