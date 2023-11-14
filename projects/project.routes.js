06ec920a4751308c43f9befeb572e8d00e177f94
Michael <gadreelazazel@gmail.com>
2023-11-14 17:47:44 +0100

    Update

--- projects/project.routes.js
+++ projects/project.routes.js
@@ -1,22 +1,23 @@
 import{
-    createProject,
-    addEmployee,
-    getOrgProject,
-    getOrgProjects,
-    getEmployeeProjects,
-    getProjectEmployees,
-    editEmployeeRole,
-    removeEmployee,
-    updateProject,
-    exportProject,
-    deleteProject
+  createProject,
+  addEmployee,
+  getOrgProject,
+  getOrgProjects,
+  getEmployeeProjects,
+  getProjectEmployees,
+  getProjectStats,
+  editEmployeeRole,
+  removeEmployee,
+  updateProject,
+  exportProject,
+  deleteProject
 } from './project.controller.js';
 import { 
-  checkOrganizationExists 
+checkOrganizationExists 
 } from '../organizations/organizations.middleware.js';
 import {
-    
-    verifyUser
+  
+  verifyUser
 } from "../middleware/authenticate.js";
 import express from "express";
 
@@ -28,6 +29,7 @@ projectRouter.post('/:orgId/project/create', createProject);
 projectRouter.post('/:orgId/project/adduser/:projectId', addEmployee);
 projectRouter.get('/:orgId/project/:projectId', getOrgProject);
 projectRouter.get('/:orgId/projects', getOrgProjects);
+projectRouter.get('/:orgId/project/stats/:projectId', getProjectStats);
 projectRouter.get('/:orgId/userprojects/:empId', getEmployeeProjects);
 projectRouter.get('/:orgId/projectusers/:projectId', getProjectEmployees);
 projectRouter.patch('/:orgId/updateprojectrole/:projectId', editEmployeeRole);
