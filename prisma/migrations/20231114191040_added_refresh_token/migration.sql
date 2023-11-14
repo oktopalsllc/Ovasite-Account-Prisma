39ac398015169b524033792b6b79a3fe9148f026
Michael <gadreelazazel@gmail.com>
2023-11-14 23:14:56 +0100

    Added formData to submission model

--- prisma/migrations/20231114191040_added_refresh_token/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Form" ADD COLUMN     "subCount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "visits" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "refreshToken" TEXT;
